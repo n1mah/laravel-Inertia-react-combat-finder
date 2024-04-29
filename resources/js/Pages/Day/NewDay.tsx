a347fec312035a455668b646268bc548172836f0
Nima Heidari <nima.heidari.n@gmail.com>
2024-04-29 09:36:58 +0330

    Feat: Implement NewDay Component

--- resources/js/Pages/Day/NewDay.tsx
+++ resources/js/Pages/Day/NewDay.tsx
@@ -8,8 +8,12 @@ export default function NewDay() {
     return (
         <MainLayout>
             <Head title="NewDay" />
-            <div className={'w-full h-full flex flex-col justify-start items-center py-[120px] overflow-y-scroll text-white'}>
-
+            <div
+                className={'w-full h-full flex flex-col justify-start items-center py-[120px] overflow-y-scroll text-white'}>
+                <h1 className={'mt-6 mb-1 font-bold text-3xl'}>New Day</h1>
+                <p className={'text-xs mb-6'}>new Event</p>
+                <div className={'bg-white h-48 w-10/12 mx-auto rounded-3xl'}></div>
+                <button className={'text-black bg-white mt-6 rounded-3xl w-10/12 mx-auto py-3 text-center '}>Start Day</button>
             </div>
         </MainLayout>
     );
