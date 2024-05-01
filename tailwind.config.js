1fc056b2991b0e9612d9c537623c1d709ed6ac14
Nima Heidari <nima.heidari.n@gmail.com>
2024-05-01 21:53:46 +0330

    Fix: Update Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,23 +16,15 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
-        // colors:{
-        //     'primary':'#000000',
-        //     'secondary':'#ffffff',
-        //     'tertiary':'#cfcfcf',
-        // },
+
         screens: {
             '2xl': {'max': '1535px'},
-            // => @media (max-width: 1535px) { ... }
 
             'xl': {'max': '1279px'},
-            // => @media (max-width: 1279px) { ... }
 
             'lg': {'max': '1023px'},
-            // => @media (max-width: 1023px) { ... }
 
             'md': {'max': '767px'},
-            // => @media (max-width: 767px) { ... }
 
             'sm': {'max': '639px'},
             // => @media (max-width: 639px) { ... }
