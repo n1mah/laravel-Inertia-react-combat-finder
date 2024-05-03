a7a5a82ca18804b8627e8f0082ac71a1487267aa
Nima Heidari <nima.heidari.n@gmail.com>
2024-05-03 11:30:30 +0330

    Feat: Add titles in new day Page

--- resources/js/Pages/Day/NewDay.tsx
+++ resources/js/Pages/Day/NewDay.tsx
@@ -11,8 +11,11 @@ export default function NewDay() {
                 className={'w-full h-full flex flex-col justify-start items-center py-[120px] overflow-y-scroll text-white'}>
                 <h1 className={'mt-6 mb-1 font-bold text-3xl'}>New Day</h1>
                 <p className={'text-xs mb-6'}>new Event</p>
-                <div className={'bg-white h-48 w-10/12 mx-auto rounded-3xl'}>
-
+                <div className={'flex items-center justify-start py-2 flex-col bg-white h-48 w-10/12 mx-auto rounded-3xl text-black'}>
+                    <h2 className={'font-bold text-xl'}>Users</h2>
+                    <p className={'text-[10px] text-center'}>Select User In Day Matches
+                        <br/>
+                        (with click)</p>
                 </div>
                 <button className={'text-black bg-white mt-6 rounded-3xl w-10/12 mx-auto py-3 text-center '}>Start Day</button>
             </div>
